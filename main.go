80abc283a97e77dbcd546ce565e7c3e99314bf8c
feedlyy <muhammadnurfadlifadli@gmail.com>
2020-03-04 15:17:40 +0700

    update validation input

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func testNumber1A (n int) int {
+func testNumber1A(n int) int {
 	if n == 1 { //base case
 		return 1
 	}
@@ -17,10 +17,10 @@ func testNumber1A (n int) int {
 			fmt.Print("\n")
 		}
 	}
-	return testNumber1A(n-1)
+	return testNumber1A(n - 1)
 }
 
-func tesNumber1B (n int) {
+func tesNumber1B(n int) {
 	//init diagonal for cross #
 	diagonalX := 1
 	diagonalY := n - 1
@@ -28,20 +28,20 @@ func tesNumber1B (n int) {
 	//count how many repeat
 	repeat := 1
 
-	for i := 1; i <= n - 1; i++ {
+	for i := 1; i <= n-1; i++ {
 		if repeat == n { //break the loop when it's reach n
 			break
 		}
 
-		if repeat == 1 || repeat == n - 1 { //if the repeatition is number 1 or the last
+		if repeat == 1 || repeat == n-1 { //if the repeatition is number 1 or the last
 			fmt.Print("#")
-			if i == n - 1 {
+			if i == n-1 {
 				fmt.Print("\n")
 			}
 		} else {
-			if i == 1 || i == diagonalX || i == diagonalY || i == n - 1 {
+			if i == 1 || i == diagonalX || i == diagonalY || i == n-1 {
 				fmt.Print("#")
-				if i == n - 1 {
+				if i == n-1 {
 					fmt.Print("\n")
 				}
 			} else {
@@ -49,7 +49,7 @@ func tesNumber1B (n int) {
 			}
 		}
 
-		if i == n - 1 { //repeat the process again
+		if i == n-1 { //repeat the process again
 			i = 0
 			repeat += 1
 			diagonalX += 1
@@ -58,12 +58,12 @@ func tesNumber1B (n int) {
 	}
 }
 
-func tesNumber2 (n int) {
+func tesNumber2(n int) {
 
 	//make a dynamic zero string
 	init := strconv.Itoa(n)
 	zero := ""
-	for x := 0; x < len(init) - 1; x++ { //len - 1 because the x start from 0 (incursion)
+	for x := 0; x < len(init)-1; x++ { //len - 1 because the x start from 0 (incursion)
 		zero += "0"
 	}
 
@@ -84,12 +84,13 @@ func main() {
 	var n, x int
 	fmt.Scan(&n, &x) //scan input number, n for tes number 1. x for tes number 2
 
-	if n <= 0 {
+	if n <= 0 || x <= 0 {
 		fmt.Print("must be a positive number or more than 0!")
 	} else {
 		fmt.Println(testNumber1A(n), "\n")
 		tesNumber1B(n)
+		fmt.Print("\n")
+		tesNumber2(x)
 	}
-	fmt.Print("\n")
-	tesNumber2(x)
+
 }
